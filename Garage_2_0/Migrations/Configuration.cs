4e8b38bae6763c78342efa921171f790e538131e
Tomas <tomas.lexicon@gmail.com>
2017-10-23 16:25:00 +0200

    Add new Seed to database

--- Garage_2_0/Migrations/Configuration.cs
+++ Garage_2_0/Migrations/Configuration.cs
@@ -24,15 +24,94 @@ namespace Garage_2_0.Migrations
               p => p.RegNo,
               new ParkedVehicle
               {
+                  Type = VehicleType.Car,
+                  RegNo = "ABC123",
+                  Color = "Blue",
+                  Brand = "Volvo",
+                  Model = "240",
+                  NumberOfWheels = 4,
+                  StartTime = DateTime.Now
+              },
+              new ParkedVehicle
+              {
+                  Type = VehicleType.Motorcycle,
+                  RegNo = "XYZ123",
+                  Color = "Red",
+                  Brand = "Honda",
+                  Model = "220SX",
+                  NumberOfWheels = 2,
+                  StartTime = DateTime.Now
+              },
+              new ParkedVehicle
+              {
+                  Type = VehicleType.Car,
+                  RegNo = "QWE345",
+                  Color = "Blue",
+                  Brand = "BMW",
+                  Model = "530i",
+                  NumberOfWheels = 4,
+                  StartTime = DateTime.Now
+              },
+              new ParkedVehicle
+              {
+                  Type = VehicleType.Bus,
+                  RegNo = "RYU567",
+                  Color = "Yellow",
+                  Brand = "Scania",
+                  Model = "550TX",
+                  NumberOfWheels = 6,
+                  StartTime = DateTime.Now
+              },
+              new ParkedVehicle
+              {
+                  Type = VehicleType.Boat,
+                  RegNo = "FY17",
+                  Color = "Black",
+                  Brand = "Fjord",
+                  Model = "997xT",
+                  NumberOfWheels = 0,
+                  StartTime = DateTime.Now
+              },
+              new ParkedVehicle
+              {
+                  Type = VehicleType.Car,
+                  RegNo = "HMM123",
+                  Color = "Green",
+                  Brand = "Ferrari",
+                  Model = "Testarossa",
+                  NumberOfWheels = 4,
+                  StartTime = DateTime.Now
+              },
+            new ParkedVehicle
+            {
                 Type = VehicleType.Car,
-                RegNo = "ABC123",
-                Color = "Blue",
-                Brand = "Volvo",
-                Model = "240",
+                RegNo = "ABC789",
+                Color = "Pink",
+                Brand = "SAAB",
+                Model = "99",
                 NumberOfWheels = 4,
                 StartTime = DateTime.Now
+            },
+              new ParkedVehicle
+              {
+                  Type = VehicleType.Motorcycle,
+                  RegNo = "IOT888",
+                  Color = "White",
+                  Brand = "Honda",
+                  Model = "440SX",
+                  NumberOfWheels = 2,
+                  StartTime = DateTime.Now
+              },
+              new ParkedVehicle
+              {
+                  Type = VehicleType.Car,
+                  RegNo = "QWE898",
+                  Color = "Blue",
+                  Brand = "Audi",
+                  Model = "S77",
+                  NumberOfWheels = 4,
+                  StartTime = DateTime.Now
               }
-
             );
 
         }
