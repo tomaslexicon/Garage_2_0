331ae99ed623fbef57a40693be63883521b789fb
Tomas <tomas.lexicon@gmail.com>
2017-10-24 10:26:54 +0200

    First version of overview

--- Garage_2_0/Controllers/ParkedVehiclesController.cs
+++ Garage_2_0/Controllers/ParkedVehiclesController.cs
@@ -45,7 +45,7 @@ namespace Garage_2_0.Controllers
             switch (sortBy.ToLower())
             {
                 case "type":
-                    return isDescending ? v.OrderByDescending(i => i.Type).ToList() : v.OrderBy(i => i.Type).ToList();
+                    return isDescending ? v.OrderByDescending(i => i.Type.ToString()).ToList() : v.OrderBy(i => i.Type.ToString()).ToList();
                 case "starttime":
                     return isDescending ? v.OrderByDescending(i => i.StartTime).ToList() : v.OrderBy(i => i.StartTime).ToList();
                 case "color":
@@ -93,36 +93,36 @@ namespace Garage_2_0.Controllers
             return View(parkedVehicle);
         }
 
-        // GET: ParkedVehicles/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            ParkedVehicle parkedVehicle = db.ParkedVehicles.Find(id);
-            if (parkedVehicle == null)
-            {
-                return HttpNotFound();
-            }
-            return View(parkedVehicle);
-        }
+        //// GET: ParkedVehicles/Edit/5
+        //public ActionResult Edit(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    ParkedVehicle parkedVehicle = db.ParkedVehicles.Find(id);
+        //    if (parkedVehicle == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(parkedVehicle);
+        //}
 
-        // POST: ParkedVehicles/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,Type,RegNo,Color,Brand,Model,NumberOfWheels,StartTime")] ParkedVehicle parkedVehicle)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(parkedVehicle).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(parkedVehicle);
-        }
+        //// POST: ParkedVehicles/Edit/5
+        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        //// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Edit([Bind(Include = "Id,Type,RegNo,Color,Brand,Model,NumberOfWheels,StartTime")] ParkedVehicle parkedVehicle)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Entry(parkedVehicle).State = EntityState.Modified;
+        //        db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
+        //    return View(parkedVehicle);
+        //}
 
         // GET: ParkedVehicles/Delete/5
         public ActionResult CheckOut(int? id)
