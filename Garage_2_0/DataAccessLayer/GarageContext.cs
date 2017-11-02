037c74a23c924d69999ea2be1fb25882b0893817
Tomas Lanquist <tomas.lexicon@gmail.com>
2017-11-02 14:42:36 +0100

    Minor changes

--- Garage_2_0/DataAccessLayer/GarageContext.cs
+++ Garage_2_0/DataAccessLayer/GarageContext.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data.Entity;
+using Garage_2_0.Models;
 
 namespace Garage_2_0.DataAccessLayer
 {
     public class GarageContext : DbContext
     {
-        public DbSet<Models.ParkedVehicle> ParkedVehicles { get; set; }
-        public DbSet<Models.Member> Members { get; set; }
-        public DbSet<Models.VehicleType> VehicleTypes { get; set; }
+        public DbSet<ParkedVehicle> ParkedVehicles { get; set; }
+        public DbSet<Member> Members { get; set; }
+        public DbSet<VehicleType> VehicleTypes { get; set; }
 
         public GarageContext() : base("Garage25Connection")
         {
